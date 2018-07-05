cedfe271fdeab24f0b0aaaf6086cb06348dcaaef
Lalita <lalitachuam@gmail.com>
2018-07-05 10:08:05 +0700

    chore(TranslationValidator) update validatation on function

--- src/TranslationValidator.php
+++ src/TranslationValidator.php
@@ -5,15 +5,24 @@ namespace CrazyFactory\Translations;
 
 class TranslationValidator
 {
-    public static function isValidKey(string $key): bool {
-        return true;
+    public static function isValidKey(string $key): bool
+    {
+        $re = '/^[a-z0-9]*([._][a-z0-9]*)*$/m';
+
+        return preg_match($re, $key);
     }
 
-    public static function isValidValue(string $value): bool {
-        return true;
+    public static function isValidValue(string $value): bool
+    {
+        $re = '/^[{<].*[>}]/m';
+
+        return !preg_match($re, $value);
     }
 
-    public static function isValidLocale(string $locale): bool {
-        return true;
+    public static function isValidLocale(string $locale): bool
+    {
+        $languages = ['cs', 'de', 'en-GB', 'en-US', 'es', 'fr', 'hr', 'it', 'nb', 'nl', 'pl', 'pt', 'fi', 'sv'];
+
+        return in_array($locale, $languages);
     }
 }
